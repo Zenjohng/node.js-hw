d7e5111210a9dd108a8c4dbe419921203e7ca8e6
Znaid04 <Znaidhr@gmail.com>
2020-08-26 23:18:06 -0400

    the first repo crashed started new

---
+++ greetings.js
@@ -0,0 +1,14 @@
+
+//assigned module.exports to determine  what’s exported from your module.
+var greeting= "name, password"
+module.exports = {
+  getName: () => {
+    return 'Zee';
+  },
+git 
+  getpassword: () => {
+    return 'ABC123';
+  },
+
+
+};
---
+++ nodehw.js
@@ -0,0 +1,17 @@
+
+//setting variables
+
+var fs = require('fs');
+var inquirer = require('inquirer');
+var http = require(./greeting);
+// function name user questions
+function userQuestions(questions){
+  console.log("Hello, Please answer the following questions:", answers);
+}// Ask questions
+var questions = [
+{
+    message: "What's your first name?",
+    type: "input",
+    name: "firstName"
+}];
+inquirer.prompt(user input, processAnswers);
